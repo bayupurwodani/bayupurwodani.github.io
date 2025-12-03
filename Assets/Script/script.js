33599ec1fc1e34a5b740d12d74b4f6d801919ba5
bayupurwodani <bayupurwodani@gmail.com>
2025-12-03 17:27:27 +0700

    Update Footer dan Menambahkan Dark Mode

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,3 +1,7 @@
 alert(
   "Selamat datang di website portofolio Bayu Purwodani! \nUntuk saat ini website hanya dapat diakses dalam versi desktop. \nMohon maaf atas ketidaknyamanannya 🙏."
 );
+
+function toggleMode() {
+  document.body.classList.toggle("dark");
+}
